105ce94061bf15106cfc199c56fa71b5eae9fe0b
Vladimir Kuzmin <vovkunch@gmail.com>
2025-09-01 18:10:35 +0500

    В customClasses добавил поле с Comparator и изменил toString, удалил компараторы, удалил один метод в TimSortTemp

--- src/sort/TimSortTemp.java
+++ src/sort/TimSortTemp.java
@@ -1,18 +1,11 @@
 package sort;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class TimSortTemp<T extends Comparable<T>> {
-
-    public List<T> sort(List<T> list) {
-        Collections.sort(list);
-        return list;
-    }
-
     public List<T> sort(List<T> list, Comparator<T> comparator) {
-        Collections.sort(list, comparator);
+        list.sort(comparator);
         return list;
     }
 }
