d43887dd4037404d68959f86776c20ef59100382
Dmitry Sheluto <shelutods@gmail.com>
2025-09-01 20:19:32 +0300

    add ObjectFileWriter

---
+++ src/fillingStrategies/file/ObjectFileWriter.java
@@ -0,0 +1,38 @@
+package fillingStrategies.file;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class ObjectFileWriter {
+    private final Path path;
+
+    public ObjectFileWriter(Path path) {
+        this.path = path;
+    }
+
+    public ObjectFileWriter() {
+        this.path = Path.of("src/resources/data.txt");
+    }
+
+    public void write(Object object) {
+        String objectData = object.toString() + "\n";
+        try {
+            Files.writeString(path, objectData, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void clearFile() {
+        try {
+            Files.writeString(path, "", StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
