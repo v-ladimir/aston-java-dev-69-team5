106a2d8a93183ceddca327b41a28a909098d10a2
Dmitry Sheluto <shelutods@gmail.com>
2025-09-01 21:20:06 +0300

    add file reader and writer

---
+++ src/fillingStrategies/file/parsers/BarrelParser.java
@@ -0,0 +1,19 @@
+package fillingStrategies.file.parsers;
+
+import customClasses.Barrel;
+import fillingStrategies.file.Parser;
+
+public class BarrelParser implements Parser<Barrel> {
+    @Override
+    public Barrel getObjectFromString(String string) {
+        String[] dataArray = string.split(",");
+        double volume = Double.parseDouble(dataArray[0].split("-")[1].trim());
+        String storedMaterial = dataArray[1].split("-")[1].trim();
+        Barrel.BarrelMaterial barrelMaterial = Barrel.BarrelMaterial.valueOf(dataArray[2].split("-")[1].trim());
+        return new Barrel.Builder()
+                .setVolume(volume)
+                .setStoredMaterial(storedMaterial)
+                .setBarrelMaterial(barrelMaterial)
+                .build();
+    }
+}
